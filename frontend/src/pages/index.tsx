f8e0e23b4ecf524728229409f8612548a90e8706
Samatar Moussa <samatarmoussa111@gmail.com>
2023-04-17 20:27:43 +0300

    installation du projet

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Seo from "@/ui/components/Seo";
+
+export default function Home() {
+  return (
+    <>
+      <Seo
+        title="Codeurs Monkeys"
+        description="Mon super projet pour apprendre Next.js"
+      />
+      <h1>Hello world</h1>
+    </>
+  );
+}
