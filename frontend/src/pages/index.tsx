cdac981a91a515a74ad29aa9c84890c08ea76171
Samatar Moussa <samatarmoussa111@gmail.com>
2023-04-24 15:16:16 +0300

    création du design system Typography

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,9 +8,28 @@ export default function Home() {
         title="Codeurs Monkeys"
         description="Mon super projet pour apprendre Next.js"
       />
-      <Typography variant="display" component="div">
-        Hello world{" "}
-      </Typography>
+      <div className="space-y-5 ">
+        <Typography variant="display" component="h1" theme="primary">
+          Facilitez vous la vie grâce à l'automatistion
+        </Typography>
+        <Typography variant="h1" component="h1" theme="secondary">
+          Facilitez vous la vie grâce à l'automatistion
+        </Typography>
+        <Typography variant="h5" component="h1" theme="gray">
+          Facilitez vous la vie grâce à l'automatistion
+        </Typography>
+        <Typography variant="body-base" component="h1" theme="gray">
+          Facilitez vous la vie grâce à l'automatistion
+        </Typography>
+        <Typography
+          variant="caption4"
+          weight="medium"
+          component="h1"
+          theme="secondary"
+        >
+          Facilitez vous la vie grâce à l'automatistion
+        </Typography>
+      </div>
     </>
   );
 }
--- frontend/src/ui/design-system/typography/Typography.tsx
+++ frontend/src/ui/design-system/typography/Typography.tsx
@@ -32,19 +32,75 @@ const Typography = ({
   className,
 }: Props) => {
   let variantStyles: string = "";
+  let colorStyles: string = "";
   switch (variant) {
     case "display":
-      variantStyles = "text-red-500";
+      variantStyles = ` text-8xl`;
       break;
     case "h1":
-      variantStyles = "text-green-500";
+      variantStyles = ` text-7xl `;
+      break;
+    case "h2":
+      variantStyles = ` text-6xl `;
+      break;
+    case "h3": // Default
+      variantStyles = ` text-5xl `;
+      break;
+    case "h4":
+      variantStyles = ` text-4xl `;
+      break;
+    case "h5":
+      variantStyles = ` text-3xl `;
+      break;
+    case "lead":
+      variantStyles = ` text-2xl `;
+      break;
+    case "body-lg":
+      variantStyles = ` text-lg `;
+      break;
+    case "body-base":
+      variantStyles = ` text-base `;
+      break;
+    case "body-sm":
+      variantStyles = ` text-sm `;
+      break;
+    case "caption1":
+      variantStyles = ` text-caption1 `;
+      break;
+    case "caption2":
+      variantStyles = ` text-caption2 `;
+      break;
+    case "caption3":
+      variantStyles = ` text-caption3 `;
+      break;
+    case "caption4":
+      variantStyles = ` text-caption4 `;
+      break;
+  }
+
+  switch (theme) {
+    case "black": // Default
+      colorStyles = "text-gray";
+      break;
+    case "gray":
+      colorStyles = "text-gray-700";
+      break;
+    case "white":
+      colorStyles = "text-white";
+      break;
+    case "primary":
+      colorStyles = "text-primary";
+      break;
+    case "secondary":
+      colorStyles = "text-secondary";
       break;
   }
   return (
     <Component
       className={clsx(
         variantStyles,
-        weight === "medium" && "font-medium ",
+        colorStyles,
+        weight === "medium" && "font-medium  ",
         className
       )}
     >
